4819503336a9e3ecef77d0890d44fc6f024e18c7
Daniel Momoh <daniel.momoh@stu.ocsb.ca>
2022-09-20 23:32:57 +0000

    Wrap lines at 72 characters.

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created by: Daniel Momoh
+# Created on: sept. 19, 2022
+# This program calculates and displays the area and perimeter of a rectangle with length 7cm and width 5cm.
+
+
+def main():
+    print("For a rectangle with length 7cm and width 4cm:")
+    print()
+    print("The area is: {} cm^2".format(7 * 3))
+    print("The Perimeter is: {} cm".format(2 * (9 + 5)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Daniel Momoh
+# Created on: Sept. 09, 19
+# This program does basic math.
+
+
+def main():
+    print("7 + 5 = {}".format(7 + 5))
+    print("13 - 9 = {}".format(13 - 9))
+    print("5 / 2 = {}".format(5 / 2))
+    print("73 - 8 / 2 = {}".format(73 - 8 / 2))
+    print("7^6 = {}".format(7 ^ 6))  # bitwise operator
+    print("9 ** 5 = {}".format(9**5))  # exponent
+    print("16 ** 0.5 = {}".format(16**0.5))  # square root
+
+
+if __name__ == "__main__":
+    main()
